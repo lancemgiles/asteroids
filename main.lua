cc95a6f6010b19330b7c48ead5a8c4849c9920bf
Lance <dogmandevices@gmail.com>
2024-03-25 00:33:23 -0500

    complete tutorial

--- main.lua
+++ main.lua
@@ -1,36 +1,11 @@
 function love.load()
 	arenaWidth = 800
 	arenaHeight = 600
-    shipX = arenaWidth / 2
-    shipY = arenaHeight / 2
-    shipAngle = 0
-    shipSpeedX = 0
-    shipSpeedY = 0
+
     shipRadius = 30
 
-    bullets = {}
-    bulletTimerLimit = 0.5
-    bulletTimer = bulletTimerLimit
+	bulletTimerLimit = 0.5
     bulletRadius = 5
-
-    asteroids = {
-    	{
-    		x = 100,
-    		y = 100,
-    	},
-    	{
-    		x = arenaWidth - 100,
-    		y = 100
-    	},
-    	{
-    		x = arenaWidth / 2,
-    		y = arenaHeight - 100,
-    	},
-    }
-
-    for asteroidIndex, asteroid in ipairs(asteroids) do
-    	asteroid.angle = love.math.random() * (2 * math.pi)
-    end
 
     asteroidStages = {
     	{
@@ -51,10 +26,38 @@ function love.load()
     	}
     }
 
-    for asteroidIndex, asteroid in ipairs(asteroids) do
-    	asteroid.angle = love.math.random() * (2 * math.pi)
-    	asteroid.stage = #asteroidStages
+    function reset()
+    	shipX = arenaWidth / 2
+    	shipY = arenaHeight / 2
+    	shipAngle = 0
+    	shipSpeedX = 0
+    	shipSpeedY = 0
+
+    	bullets = {}
+	    bulletTimer = bulletTimerLimit
+
+	    asteroids = {
+	    	{
+	    		x = 100,
+	    		y = 100,
+	    	},
+	    	{
+	    		x = arenaWidth - 100,
+	    		y = 100
+	    	},
+	    	{
+	    		x = arenaWidth / 2,
+	    		y = arenaHeight - 100,
+	    	},
+	    }
+
+	    for asteroidIndex, asteroid in ipairs(asteroids) do
+	    	asteroid.angle = love.math.random() * (2 * math.pi)
+	    	asteroid.stage = #asteroidStages
+	    end
     end
+
+    reset()
 end
 
 
@@ -156,13 +159,13 @@ function love.update(dt)
 			shipX, shipY, shipRadius,
 			asteroid.x, asteroid.y, asteroidStages[asteroid.stage].radius
 		) then
-			love.load()
+			reset()
 			break
 		end
 	end
 
 	if #asteroids == 0 then
-		love.load()
+		reset()
 	end
 end
 
